657fda8cfb54f958c6e9f0b8d56d69cad6e28546
bobcat88 <98208683+bobcat88@users.noreply.github.com>
2024-01-08 08:22:03 +0000

    added Take Vocal command
    still needs Google API setup before it works. - next thing to look into

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import datetime
 
 import pyttsx3
 
+import speech_recognition_google as sr
+
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 index = 1
@@ -17,12 +19,10 @@ engine.setProperty('voice', voices[1].id)
 
 author = "Jo"
 
-
 # define the speak function
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
-
 
 # define the greetings function
 def greetings():
@@ -35,7 +35,21 @@ def greetings():
         speak(f"Good Evening {author}")
     speak(f"what would you like me to do for you today?")
 
-
+# define the take command function - need API setup to work 
+def take_command():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+      print("listening...")
+      r.pause_threshold = 1  # seconds before the input is taken after last spoken word
+      audio = r.listen(source)
+    try:
+      print("Recognizing...")
+      query = r.recognize_google(audio, language='en-us')
+      print(f"user said: {query}\n")
+    except Exception as e:
+        print(f"Sorry {author}, I did not get that, could you repeat please?...")
+        return "None"
+    return query
 
 if __name__ == "__main__":
     speak(f"welcome{author}, I am nina")
